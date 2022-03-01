ddf4934bc1612aff9af90206ef6ff6cca2eec510
Abhishek Mishra <abhi28_lko@yahoo.co.in>
2022-03-01 18:01:36 +0530

    initial changes

---
+++ src/main/java/com/example/rentcomparator/Buy.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package main.java.com.example.rentcomparator;
+
+/**
+ * @author AbhishekMishra
+ *
+ */
+public class Buy {
+	
+	private double flatPrice;
+	private double purchaseYear;
+	private double monthlyMaintenance;
+	private double maintenaceIncreaseRate;
+	
+	public double getFlatPrice() {
+		return flatPrice;
+	}
+	public void setFlatPrice(double flatPrice) {
+		this.flatPrice = flatPrice;
+	}
+	public double getPurchaseYear() {
+		return purchaseYear;
+	}
+	public void setPurchaseYear(double purchaseYear) {
+		this.purchaseYear = purchaseYear;
+	}
+	public double getMonthlyMaintenance() {
+		return monthlyMaintenance;
+	}
+	public void setMonthlyMaintenance(double monthlyMaintenance) {
+		this.monthlyMaintenance = monthlyMaintenance;
+	}
+	public double getMaintenaceIncreaseRate() {
+		return maintenaceIncreaseRate;
+	}
+	public void setMaintenaceIncreaseRate(double maintenaceIncreaseRate) {
+		this.maintenaceIncreaseRate = maintenaceIncreaseRate;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/rentcomparator/Rent.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package main.java.com.example.rentcomparator;
+
+/**
+ * @author AbhishekMishra
+ *
+ */
+public class Rent {
+	
+	private double flatMonthlyRent;
+	private double rentedYear;
+	private double monthlyMaintenance;
+	private double increaseRate;
+
+	public double getFlatMonthlyRent() {
+		return flatMonthlyRent;
+	}
+
+	public void setFlatMonthlyRent(double flatMonthlyRent) {
+		this.flatMonthlyRent = flatMonthlyRent;
+	}
+
+	public double getRentedYear() {
+		return rentedYear;
+	}
+
+	public void setRentedYear(double rentedYear) {
+		this.rentedYear = rentedYear;
+	}
+
+	public double getMonthlyMaintenance() {
+		return monthlyMaintenance;
+	}
+
+	public void setMonthlyMaintenance(double monthlyMaintenance) {
+		this.monthlyMaintenance = monthlyMaintenance;
+	}
+
+	public double getIncreaseRate() {
+		return increaseRate;
+	}
+
+	public void setIncreaseRate(double increaseRate) {
+		this.increaseRate = increaseRate;
+	}
+	
+}
